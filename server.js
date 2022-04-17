44b8fc434b07e9e54074058e3d965be8e2cdfd81
mkanarr <mkanarr@tamu.edu>
2022-04-17 15:52:54 -0500

    fixed refresh callback

--- server.js
+++ server.js
@@ -1,6 +1,8 @@
 const express = require('express');
 const app = express();
 const nodeMailer = require('nodemailer');
+
+require('dotenv').config();
 // port 5000 for dev
 const PORT = process.env.PORT || 5000;
 const { google } = require('googleapis');
@@ -22,38 +24,43 @@ app.get('/', (req, res) => {
 app.post('/', async (req, res) => {
     console.log(req.body);
 
-    const accessToken = await oAuth2Client.getAccessToken(); 
+    try {
+        const accessToken = await oAuth2Client.getAccessToken(); 
 
-    const transporter = nodeMailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
-        auth: {
-            type: 'OAuth2',
-            user: process.env.EMAIL,
-            clientId: process.env.CLIENT_ID,
-            clientSecret: process.env.CLIENT_SECRET,
-            refreshToken: process.env.REF_TOKEN,
-            accessToken: accessToken
-        }
-    });
-
-    const mailOptions = {
-        from: req.body.email,
-        to: process.env.EMAIL,
-        subject: `Message from ${req.body.email} - ${req.body.subject}`,
-        text: req.body.message
-    };
-
-    transporter.sendMail(mailOptions, (error, info) => {
-        if (error) {
-            console.log(error);
-            res.send('error');
-        }
-
-        console.log('Email sent ' + info.response);
-        res.send('success');
-    });
+        const transporter = nodeMailer.createTransport({
+            host: 'smtp.gmail.com',
+            port: 465,
+            secure: true,
+            auth: {
+                type: 'OAuth2',
+                user: process.env.EMAIL,
+                clientId: process.env.CLIENT_ID,
+                clientSecret: process.env.CLIENT_SECRET,
+                refreshToken: process.env.REF_TOKEN,
+                accessToken: accessToken
+            }
+        });
+    
+        const mailOptions = {
+            from: req.body.email,
+            to: process.env.EMAIL,
+            subject: `Message from ${req.body.email} - ${req.body.subject}`,
+            text: req.body.message
+        };
+    
+        transporter.sendMail(mailOptions, (error, info) => {
+            if (error) {
+                console.log(error);
+                res.send('error');
+            }
+    
+            console.log('Email sent ' + info.response);
+            res.send('success');
+        });
+    } catch(error) {
+        console.log(error)
+        return error
+    }
 });
 
 app.listen(PORT, () => {
